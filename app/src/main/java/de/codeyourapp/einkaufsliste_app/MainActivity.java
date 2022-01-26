f5a0082d404e20a750546abe4bcb9a70dcbdfc25
lukas <lukas.emmenecker@gmx.de>
2022-01-26 20:43:42 +0100

    Vorbereitung Suchfunktion

--- app/src/main/java/de/codeyourapp/einkaufsliste_app/MainActivity.java
+++ app/src/main/java/de/codeyourapp/einkaufsliste_app/MainActivity.java
@@ -2,13 +2,32 @@ package de.codeyourapp.einkaufsliste_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";    //L:Um Logeinträge zu machen (Fehlersuche)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Suchfunktion = (Button) findViewById(R.id.button_add_product);
+
+        Suchfunktion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG,"onClick: Clicked Suchfunktion");
+
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
